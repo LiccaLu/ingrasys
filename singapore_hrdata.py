f04cdbfecd5deef6002946880d2581b0a946be27
LiccaLu <tinghsuan.lu23@gmail.com>
2026-08-14 14:49:44 +0800

    Update singapore_hrdata.py

--- singapore_hrdata.py
+++ singapore_hrdata.py
@@ -3378,7 +3378,7 @@ The accompanying table includes:
                                 f"{x:.1f}%"
                                 for x in ot_summary["OT Ratio"]
                             ],
-                            textposition="top right",
+                            textposition="middle right",
                             textfont=dict(
                                 size=11,
                                 color="#ED6A2C",
