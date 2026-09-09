c5a64122e1dca7435f124821fe0f0461893047f8
LiccaLu <tinghsuan.lu23@gmail.com>
2026-09-09 11:35:40 +0800

    Update singapore_hrdata.py

--- singapore_hrdata.py
+++ singapore_hrdata.py
@@ -1885,7 +1885,7 @@ if page == "01  Upload":
     if recruitment_files:
     
         # Change this whenever recruitment parsing logic changes.
-        RECRUITMENT_PARSER_VERSION = "v9"
+        RECRUITMENT_PARSER_VERSION = "v8"
     
         current_signature = (
             RECRUITMENT_PARSER_VERSION,
